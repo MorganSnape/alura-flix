5e62a7c52a9d63357b7098a9b42c134b8f627813
Gabriela Ferrer <gabriellangeles@outlook.com>
2025-01-19 15:47:21 -0500

    Se crea el footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="flex flex-col items-center justify-center w-full h-24 bg-black border-t-4 border-[#2271d1] ">
+     <img src="/Logo.svg" alt="Logo" className=" h-1/2 object-cover object-center"/>
+    </footer>
+  );
+}
